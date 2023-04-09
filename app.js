a9bb7c696e51b744797565603aaf48364ad071a0
loyalbamboo <61188803+HARY-X@users.noreply.github.com>
2023-04-09 15:20:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,201 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'password',
+  database: 'Mod'
+});
+
+app.use(
+  session({
+    secret: 'my_secret_key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use((req, res, next) => {
+  if (req.session.userId === undefined) {
+    res.locals.username = 'guest';
+    res.locals.isLoggedIn = false;
+  } else {
+    const username = req.session.username;
+    res.locals.username = req.session.username;
+    res.locals.isLoggedIn = true;
+    const userId = req.session.id
+  }
+  next();
+});
+
+app.get('/test',(req,res)=>{
+  res.render('test.ejs');
+});
+
+app.get('/',(req,res)=>{
+  res.render('top.ejs');
+});
+
+app.get('/home', (req, res) => {
+  connection.query(
+    'SELECT * FROM posts ORDER BY id DESC LIMIT 100',
+    (error, results) => {
+      if(error) {
+        console.log(error);
+      } else {
+        res.render('home.ejs', { posts: results });
+      }
+    }
+  );
+});
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+app.post('/post', (req,res) => {
+  const postContent = req.body.postContent;
+  const postTime = new Date();
+  const id = 111;
+  const errors = [];
+
+  connection.query(
+    'INSERT INTO posts (user_id, content, created_at) VALUES (?, ?, ?, ?)',
+    [id, postContent, postTime],
+    (error, results) => {
+      if (error) {
+        errors.push('投稿に失敗しました');
+        res.render('home.ejs', { errors: errors });
+      } else {
+        res.redirect('/home');
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
+    }
+  );
+  
+})
+
+app.get('/projects/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM projects WHERE id = ?',
+    [id],
+    (error, results) => {
+      res.render('project.ejs', { projects: results[0] });
+    }
+  );
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs', { errors: [] });
+});
+
+app.post('/signup', 
+  (req, res, next) => {
+    console.log('入力値の空チェック');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = [];
+
+    if (username === '') {
+      errors.push('ユーザー名が空です');
+    }
+
+    if (email === '') {
+      errors.push('メールアドレスが空です');
+    }
+
+    if (password === '') {
+      errors.push('パスワードが空です');
+    }
+
+    if (errors.length > 0) {
+      res.render('signup.ejs', { errors: errors });
+    } else {
+      next();
+    }
+  },
+  (req, res, next) => {
+    console.log('メールアドレスの重複チェック');
+    const email = req.body.email;
+    const errors = [];
+    connection.query(
+      'SELECT * FROM users WHERE email = ?',
+      [email],
+      (error, results) => {
+        if (error) {
+          console.error('エラーが発生しました:', error);
+          return res.status(500).send('内部エラーが発生しました');
+        }
+        if (results && results.length > 0) {
+          errors.push('ユーザー登録に失敗しました');
+          res.render('signup.ejs', { errors: errors });
+        } else {
+          next();
+        }
       }
-      body {
-        background: white;
+    );
+  },
+  (req, res) => {
+    console.log('ユーザー登録');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (error, hash) => {
+      connection.query(
+        'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+        [username, email, hash],
+        (error, results) => {
+          req.session.userId = results.insertId;
+          req.session.username = username;
+          res.redirect('/home');
+        }
+      );
+    });
+  }
+);
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (error, results) => {
+      if (results.length > 0) {
+        const plain = req.body.password;
+        const hash = results[0].password;
+        bcrypt.compare(plain, hash, (error, isEqual) => {
+          if(isEqual){
+            req.session.userId = results[0].id;
+            req.session.username = results[0].username;
+            res.redirect('/home');
+          } else {
+            res.redirect('/login');
+          }
+        })
+      } else {
+        res.redirect('/login');
       }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    res.redirect('/list');
+  })
+})
+
+app.listen(3000);
